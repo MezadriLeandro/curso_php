b8e5c12d9bf89818da1e2bf41c2e22c99210f105
MezadriLeandro <leandromezadri@hotmail.com>
2024-06-18 22:05:32 -0300

    Primeiro comit no github

---
+++ aula1.php
@@ -0,0 +1,42 @@
+<?php
+/* aula de numero 1 dia 18-06-2024
+turma BGV 05000
+conteudos: echo, comentarios variaveis 
+por: Leandro */ 
+//Ete é um comentário do codigo não sera exibido em tela 
+echo "Buenaasssss!!!!<br>";
+echo "<br>";
+
+echo "Boa Noite <br>";
+
+# outro tipo de comentario 
+/*mais um tipo de comentario */
+
+$texto = "<br>Pequeno texto que você está lendo do livro do pequeno Maluquinho!<br>";
+echo $texto;
+
+echo "<br>";
+
+$outraVariavel = "outro grande texto"; // aqui é um comentario 
+echo $outraVariavel;
+
+echo "<br>Calculos matematicos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*180;
+echo "<br>";
+echo 50/7;
+echo "<br>";
+echo 4%2;
+echo "<br>";
+
+echo "<br>";
+echo 5%2;
+echo "<br>";
+
+
+
+
+?>
